09901aa5735c434f4add3ad11ee613ab3a636f2d
Nathan Baulch <nathan.baulch@gmail.com>
2015-01-17 16:40:33 +1100

    Convenience DB views with commonly joined tables

--- database/MSSQL.sql
+++ database/MSSQL.sql
@@ -17,21 +17,21 @@ ALTER DATABASE [FitBot] SET READ_COMMITTED_SNAPSHOT ON
 GO
 
 CREATE TABLE [User] (
-	[Id] [bigint] PRIMARY KEY NOT NULL,
-	[Username] [nvarchar] (100) NOT NULL,
-	[InsertDate] [datetime2] NOT NULL,
-	UNIQUE([Username])
+  [Id] [bigint] PRIMARY KEY NOT NULL,
+  [Username] [nvarchar] (100) NOT NULL,
+  [InsertDate] [datetime2] NOT NULL,
+  UNIQUE([Username])
 )
 GO
 
 CREATE TABLE [Workout] (
-	[Id] [bigint] PRIMARY KEY NOT NULL,
-	[UserId] [bigint] NOT NULL,
-	[Date] [datetime2] NULL,
-	[Points] [int] NULL,
-	[InsertDate] [datetime2] NOT NULL,
-	[ActivitiesHash] [int] NOT NULL,
-	FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+  [Id] [bigint] PRIMARY KEY NOT NULL,
+  [UserId] [bigint] NOT NULL,
+  [Date] [datetime2] NULL,
+  [Points] [int] NULL,
+  [InsertDate] [datetime2] NOT NULL,
+  [ActivitiesHash] [int] NOT NULL,
+  FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
 )
 GO
 
@@ -39,14 +39,14 @@ CREATE INDEX [IX_Workout_UserId] ON [Workout] ([UserId])
 GO
 
 CREATE TABLE [Activity] (
-	[Id] [bigint] PRIMARY KEY IDENTITY NOT NULL,
-	[WorkoutId] [bigint] NOT NULL,
-	[Sequence] [int] NOT NULL,
-	[Name] [nvarchar] (100) NOT NULL,
-	[Group] [nvarchar] (100) NULL,
-	[Note] [text] NULL,
-	UNIQUE ([WorkoutId], [Sequence]),
-	FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
+  [Id] [bigint] PRIMARY KEY IDENTITY NOT NULL,
+  [WorkoutId] [bigint] NOT NULL,
+  [Sequence] [int] NOT NULL,
+  [Name] [nvarchar] (100) NOT NULL,
+  [Group] [nvarchar] (100) NULL,
+  [Note] [text] NULL,
+  UNIQUE ([WorkoutId], [Sequence]),
+  FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
 )
 GO
 
@@ -54,21 +54,21 @@ CREATE INDEX [IX_Activity_WorkoutId] ON [Activity] ([WorkoutId])
 GO
 
 CREATE TABLE [Set] (
-	[Id] [bigint] PRIMARY KEY IDENTITY NOT NULL,
-	[ActivityId] [bigint] NOT NULL,
-	[Sequence] [int] NOT NULL,
-	[Points] [int] NULL,
-	[Distance] [decimal](19, 2) NULL,
-	[Duration] [decimal](9, 2) NULL,
-	[Speed] [decimal](9, 2) NULL,
-	[Repetitions] [decimal](9, 2) NULL,
-	[Weight] [decimal](9, 2) NULL,
-	[HeartRate] [decimal](9, 2) NULL,
-	[Incline] [decimal](9, 2) NULL,
-	[Difficulty] [nvarchar] (100) NULL,
-	[IsPr] [bit] NOT NULL,
-	UNIQUE ([ActivityId], [Sequence]),
-	FOREIGN KEY ([ActivityId]) REFERENCES [Activity] ([Id]) ON DELETE CASCADE
+  [Id] [bigint] PRIMARY KEY IDENTITY NOT NULL,
+  [ActivityId] [bigint] NOT NULL,
+  [Sequence] [int] NOT NULL,
+  [Points] [int] NULL,
+  [Distance] [decimal](19, 2) NULL,
+  [Duration] [decimal](9, 2) NULL,
+  [Speed] [decimal](9, 2) NULL,
+  [Repetitions] [decimal](9, 2) NULL,
+  [Weight] [decimal](9, 2) NULL,
+  [HeartRate] [decimal](9, 2) NULL,
+  [Incline] [decimal](9, 2) NULL,
+  [Difficulty] [nvarchar] (100) NULL,
+  [IsPr] [bit] NOT NULL,
+  UNIQUE ([ActivityId], [Sequence]),
+  FOREIGN KEY ([ActivityId]) REFERENCES [Activity] ([Id]) ON DELETE CASCADE
 )
 GO
 
@@ -76,23 +76,63 @@ CREATE INDEX [IX_Set_ActivityId] ON [Set] ([ActivityId])
 GO
 
 CREATE TABLE [Achievement] (
-	[Id] [bigint] PRIMARY KEY IDENTITY NOT NULL,
-	[WorkoutId] [bigint] NOT NULL,
-	[Type] [nvarchar] (100) NOT NULL,
-	[Group] [nvarchar] (100) NULL,
-	[Activity] [nvarchar] (100) NULL,
-	[Distance] [decimal](19, 2) NULL,
-	[Duration] [decimal](9, 2) NULL,
-	[Speed] [decimal](9, 2) NULL,
-	[Repetitions] [decimal](9, 2) NULL,
-	[Weight] [decimal](9, 2) NULL,
-	[CommentId] [bigint] NULL,
-	[CommentText] [text] NULL,
-	[IsPropped] [bit] NOT NULL,
-	[InsertDate] [datetime2] NOT NULL,
-	FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
+  [Id] [bigint] PRIMARY KEY IDENTITY NOT NULL,
+  [WorkoutId] [bigint] NOT NULL,
+  [Type] [nvarchar] (100) NOT NULL,
+  [Group] [nvarchar] (100) NULL,
+  [Activity] [nvarchar] (100) NULL,
+  [Distance] [decimal](19, 2) NULL,
+  [Duration] [decimal](9, 2) NULL,
+  [Speed] [decimal](9, 2) NULL,
+  [Repetitions] [decimal](9, 2) NULL,
+  [Weight] [decimal](9, 2) NULL,
+  [CommentId] [bigint] NULL,
+  [CommentText] [text] NULL,
+  [IsPropped] [bit] NOT NULL,
+  [InsertDate] [datetime2] NOT NULL,
+  FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
 )
 GO
 
 CREATE INDEX [IX_Achievement_WorkoutId] ON [Achievement] ([WorkoutId])
+GO
+
+CREATE VIEW [WorkoutView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash]
+FROM [User] u, [Workout] w
+WHERE u.[Id] = w.[UserId]
+GO
+
+CREATE VIEW [ActivityView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash],
+       a.[Id] [ActivityId], a.[Sequence], a.[Name], a.[Group], a.[Note]
+FROM [User] u, [Workout] w, [Activity] a
+WHERE u.[Id] = w.[UserId]
+  AND w.[Id] = a.[WorkoutId]
+GO
+
+CREATE VIEW [SetView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points] [WorkoutPoints], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash],
+       a.[Id] [ActivityId], a.[Sequence] [ActivitySequence], a.[Name], a.[Group], a.[Note],
+       s.[Id] [SetId], s.[Sequence] [SetSequence], s.[Points] [SetPoints],
+       s.[Distance], s.[Duration], s.[Speed], s.[Repetitions], s.[Weight],
+       s.[HeartRate], s.[Incline], s.[Difficulty], s.[IsPr]
+FROM [User] u, [Workout] w, [Activity] a, [Set] s
+WHERE u.[Id] = w.[UserId]
+  AND w.[Id] = a.[WorkoutId]
+  AND a.[Id] = s.[ActivityId]
+GO
+
+CREATE VIEW [AchievementView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash],
+       a.[Id] [AchievementId], a.[Type], a.[Group], a.[Activity],
+       a.[Distance], a.[Duration], a.[Speed], a.[Repetitions], a.[Weight],
+       a.[CommentId], a.[CommentText], a.[IsPropped], a.[InsertDate] [AchievementInsertDate]
+FROM [User] u, [Workout] w, [Achievement] a
+WHERE u.[Id] = w.[UserId]
+  AND w.[Id] = a.[WorkoutId]
 GO
--- database/SQLite.sql
+++ database/SQLite.sql
@@ -8,74 +8,118 @@ DROP TABLE IF EXISTS [Set];
 
 DROP TABLE IF EXISTS [Achievement];
 
+DROP VIEW IF EXISTS [WorkoutView];
+
+DROP VIEW IF EXISTS [ActivityView];
+
+DROP VIEW IF EXISTS [SetView];
+
+DROP VIEW IF EXISTS [AchievementView];
+
 CREATE TABLE [User] (
-	[Id] [bigint] PRIMARY KEY NOT NULL,
-	[Username] [nvarchar] (100) NOT NULL,
-	[InsertDate] [datetime2] NOT NULL,
-	UNIQUE([Username])
+  [Id] [bigint] PRIMARY KEY NOT NULL,
+  [Username] [nvarchar] (100) NOT NULL,
+  [InsertDate] [datetime2] NOT NULL,
+  UNIQUE([Username])
 );
 
 CREATE TABLE [Workout] (
-	[Id] [bigint] PRIMARY KEY NOT NULL,
-	[UserId] [bigint] NOT NULL,
-	[Date] [datetime2] NULL,
-	[Points] [int] NULL,
-	[InsertDate] [datetime2] NOT NULL,
-	[ActivitiesHash] [int] NOT NULL,
-	FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+  [Id] [bigint] PRIMARY KEY NOT NULL,
+  [UserId] [bigint] NOT NULL,
+  [Date] [datetime2] NULL,
+  [Points] [int] NULL,
+  [InsertDate] [datetime2] NOT NULL,
+  [ActivitiesHash] [int] NOT NULL,
+  FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
 );
 
 CREATE INDEX [IX_Workout_UserId] ON [Workout] ([UserId]);
 
 CREATE TABLE [Activity] (
-	[Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	[WorkoutId] [bigint] NOT NULL,
-	[Sequence] [int] NOT NULL,
-	[Name] [nvarchar] (100) NOT NULL,
-	[Group] [nvarchar] (100) NULL,
-	[Note] [text] NULL,
-	UNIQUE ([WorkoutId], [Sequence]),
-	FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
+  [Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  [WorkoutId] [bigint] NOT NULL,
+  [Sequence] [int] NOT NULL,
+  [Name] [nvarchar] (100) NOT NULL,
+  [Group] [nvarchar] (100) NULL,
+  [Note] [text] NULL,
+  UNIQUE ([WorkoutId], [Sequence]),
+  FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
 );
 
 CREATE INDEX [IX_Activity_WorkoutId] ON [Activity] ([WorkoutId]);
 
 CREATE TABLE [Set] (
-	[Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	[ActivityId] [bigint] NOT NULL,
-	[Sequence] [int] NOT NULL,
-	[Points] [int] NULL,
-	[Distance] [decimal](19, 2) NULL,
-	[Duration] [decimal](9, 2) NULL,
-	[Speed] [decimal](9, 2) NULL,
-	[Repetitions] [decimal](9, 2) NULL,
-	[Weight] [decimal](9, 2) NULL,
-	[HeartRate] [decimal](9, 2) NULL,
-	[Incline] [decimal](9, 2) NULL,
-	[Difficulty] [nvarchar] (100) NULL,
-	[IsPr] [bit] NOT NULL,
-	UNIQUE ([ActivityId], [Sequence]),
-	FOREIGN KEY ([ActivityId]) REFERENCES [Activity] ([Id]) ON DELETE CASCADE
+  [Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  [ActivityId] [bigint] NOT NULL,
+  [Sequence] [int] NOT NULL,
+  [Points] [int] NULL,
+  [Distance] [decimal](19, 2) NULL,
+  [Duration] [decimal](9, 2) NULL,
+  [Speed] [decimal](9, 2) NULL,
+  [Repetitions] [decimal](9, 2) NULL,
+  [Weight] [decimal](9, 2) NULL,
+  [HeartRate] [decimal](9, 2) NULL,
+  [Incline] [decimal](9, 2) NULL,
+  [Difficulty] [nvarchar] (100) NULL,
+  [IsPr] [bit] NOT NULL,
+  UNIQUE ([ActivityId], [Sequence]),
+  FOREIGN KEY ([ActivityId]) REFERENCES [Activity] ([Id]) ON DELETE CASCADE
 );
 
 CREATE INDEX [IX_Set_ActivityId] ON [Set] ([ActivityId]);
 
 CREATE TABLE [Achievement] (
-	[Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	[WorkoutId] [bigint] NOT NULL,
-	[Type] [nvarchar] (100) NOT NULL,
-	[Group] [nvarchar] (100) NULL,
-	[Activity] [nvarchar] (100) NULL,
-	[Distance] [decimal](19, 2) NULL,
-	[Duration] [decimal](9, 2) NULL,
-	[Speed] [decimal](9, 2) NULL,
-	[Repetitions] [decimal](9, 2) NULL,
-	[Weight] [decimal](9, 2) NULL,
-	[CommentId] [bigint] NULL,
-	[CommentText] [text] NULL,
-	[IsPropped] [bit] NOT NULL,
-	[InsertDate] [datetime2] NOT NULL,
-	FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
+  [Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  [WorkoutId] [bigint] NOT NULL,
+  [Type] [nvarchar] (100) NOT NULL,
+  [Group] [nvarchar] (100) NULL,
+  [Activity] [nvarchar] (100) NULL,
+  [Distance] [decimal](19, 2) NULL,
+  [Duration] [decimal](9, 2) NULL,
+  [Speed] [decimal](9, 2) NULL,
+  [Repetitions] [decimal](9, 2) NULL,
+  [Weight] [decimal](9, 2) NULL,
+  [CommentId] [bigint] NULL,
+  [CommentText] [text] NULL,
+  [IsPropped] [bit] NOT NULL,
+  [InsertDate] [datetime2] NOT NULL,
+  FOREIGN KEY ([WorkoutId]) REFERENCES [Workout] ([Id]) ON DELETE CASCADE
 );
 
 CREATE INDEX [IX_Achievement_WorkoutId] ON [Achievement] ([WorkoutId]);
+
+CREATE VIEW [WorkoutView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash]
+FROM [User] u, [Workout] w
+WHERE u.[Id] = w.[UserId];
+
+CREATE VIEW [ActivityView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash],
+       a.[Id] [ActivityId], a.[Sequence], a.[Name], a.[Group], a.[Note]
+FROM [User] u, [Workout] w, [Activity] a
+WHERE u.[Id] = w.[UserId]
+  AND w.[Id] = a.[WorkoutId];
+
+CREATE VIEW [SetView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points] [WorkoutPoints], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash],
+       a.[Id] [ActivityId], a.[Sequence] [ActivitySequence], a.[Name], a.[Group], a.[Note],
+       s.[Id] [SetId], s.[Sequence] [SetSequence], s.[Points] [SetPoints],
+       s.[Distance], s.[Duration], s.[Speed], s.[Repetitions], s.[Weight],
+       s.[HeartRate], s.[Incline], s.[Difficulty], s.[IsPr]
+FROM [User] u, [Workout] w, [Activity] a, [Set] s
+WHERE u.[Id] = w.[UserId]
+  AND w.[Id] = a.[WorkoutId]
+  AND a.[Id] = s.[ActivityId];
+
+CREATE VIEW [AchievementView] AS
+SELECT u.[Id] [UserId], u.[Username], u.[InsertDate] [UserInsertDate],
+       w.[Id] [WorkoutId], w.[Date], w.[Points], w.[InsertDate] [WorkoutInsertDate], w.[ActivitiesHash],
+       a.[Id] [AchievementId], a.[Type], a.[Group], a.[Activity],
+       a.[Distance], a.[Duration], a.[Speed], a.[Repetitions], a.[Weight],
+       a.[CommentId], a.[CommentText], a.[IsPropped], a.[InsertDate] [AchievementInsertDate]
+FROM [User] u, [Workout] w, [Achievement] a
+WHERE u.[Id] = w.[UserId]
+  AND w.[Id] = a.[WorkoutId];
