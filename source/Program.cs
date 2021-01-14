c49888dc9e7621c22c291a0f94e002451db8136b
Nathan Baulch <nathan.baulch@gmail.com>
2021-01-14 16:23:42 +1100

    Remove SimpleInjector

--- source/Program.cs
+++ source/Program.cs
@@ -1,4 +1,7 @@
-﻿using FitBot.Logging;
+﻿using FitBot.Achievements;
+using FitBot.Logging;
+using FitBot.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,6 +13,19 @@ namespace FitBot
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((context, services) => services
                     .Configure<EmailLoggerOptions>(context.Configuration.GetSection("Email"))
+                    .AddSingleton(context.Configuration.GetSection("Database").Get<DatabaseOptions>())
+                    .AddSingleton(context.Configuration.GetSection("Fitocracy").Get<FitocracyOptions>())
+                    .AddSingleton<IAchievementPushService, AchievementPushService>()
+                    .AddSingleton<IAchievementService, AchievementService>()
+                    .AddSingleton<IActivityGroupingService, ActivityGroupingService>()
+                    .AddSingleton<IDatabaseService, DatabaseService>()
+                    .AddSingleton<IFitocracyService, FitocracyService>()
+                    .AddSingleton<IScrapingService, ScrapingService>()
+                    .AddSingleton<IUserPullService, UserPullService>()
+                    .AddSingleton<IWebRequestService, WebRequestService>()
+                    .AddSingleton<IWorkoutPullService, WorkoutPullService>()
+                    .Scan(scan => scan.FromEntryAssembly().AddClasses(filter => filter.AssignableTo<IAchievementProvider>()).As<IAchievementProvider>())
+                    .Decorate<IWebRequestService, ThrottledWebRequestDecorator>()
                     .AddHostedService<Worker>())
                 .ConfigureLogging(builder => builder.AddEmailLogger())
                 .Build()
--- source/Worker.cs
+++ source/Worker.cs
@@ -1,117 +1,49 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using FitBot.Achievements;
 using FitBot.Services;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using SimpleInjector;
 
 namespace FitBot
 {
     public class Worker : BackgroundService
     {
-        private readonly IConfiguration _configuration;
+        private readonly IUserPullService _userPull;
+        private readonly IWorkoutPullService _workoutPull;
+        private readonly IAchievementService _achieveService;
+        private readonly IAchievementPushService _achievementPush;
         private readonly ILogger<Worker> _logger;
-        private Container _container;
 
-        public Worker(IConfiguration configuration, ILogger<Worker> logger)
+        public Worker(
+            IUserPullService userPull,
+            IWorkoutPullService workoutPull,
+            IAchievementService achieveService,
+            IAchievementPushService achievementPush,
+            ILogger<Worker> logger)
         {
-            _configuration = configuration;
+            _userPull = userPull;
+            _workoutPull = workoutPull;
+            _achieveService = achieveService;
+            _achievementPush = achievementPush;
             _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _container = new Container();
-
-            _container.Register<IAchievementPushService, AchievementPushService>();
-            _container.Register<IAchievementService, AchievementService>();
-            _container.Register<IActivityGroupingService, ActivityGroupingService>();
-            _container.Register(() => _configuration.GetSection("Database").Get<DatabaseOptions>(), Lifestyle.Singleton);
-            _container.Register<IDatabaseService, DatabaseService>();
-            _container.Register(() => _configuration.GetSection("Fitocracy").Get<FitocracyOptions>(), Lifestyle.Singleton);
-            _container.Register<IFitocracyService, FitocracyService>();
-            _container.Register<IScrapingService, ScrapingService>();
-            _container.Register<IUserPullService, UserPullService>();
-            _container.Register<IWebRequestService, WebRequestService>();
-            _container.Register<IWorkoutPullService, WorkoutPullService>();
-
-            _container.Collection.Register<IAchievementProvider>(
-                GetType().Assembly.GetTypes()
-                    .Where(typeof(IAchievementProvider).IsAssignableFrom)
-                    .Where(type => type.IsClass));
-
-            _container.RegisterDecorator<IWebRequestService, ThrottledWebRequestDecorator>();
-
-            _container.Verify();
-
-            await Run(stoppingToken);
-        }
-
-        private async Task Run(CancellationToken stoppingToken)
-        {
-            var throttler = (ThrottledWebRequestDecorator) _container.GetInstance<IWebRequestService>();
-            var errorBackoff = 0;
-
             while (!stoppingToken.IsCancellationRequested)
             {
                 var start = DateTime.Now;
 
-                try
+                foreach (var user in await _userPull.Pull(stoppingToken))
                 {
-                    await Execute(_container, stoppingToken);
-                    errorBackoff = 0;
-                }
-                catch (Exception ex)
-                {
-                    var aggregateEx = (ex as AggregateException)?.Flatten() ?? new AggregateException(ex);
-
-                    foreach (var inner in aggregateEx.InnerExceptions)
-                    {
-                        if (inner is OperationCanceledException)
-                        {
-                            return;
-                        }
-
-                        _logger.LogError(inner.ToString());
-                    }
-
-                    await Task.Delay(TimeSpan.FromSeconds((int) Math.Pow(2, errorBackoff)), stoppingToken);
-                    errorBackoff++;
+                    var workouts = await _workoutPull.Pull(user, stoppingToken);
+                    var achievements = await _achieveService.Process(user, workouts, stoppingToken);
+                    await _achievementPush.Push(achievements, stoppingToken);
+                    stoppingToken.ThrowIfCancellationRequested();
                 }
 
-                var elapsed = DateTime.Now - start;
-                _logger.LogInformation("Processing time: " + elapsed);
-
-                if (errorBackoff == 0)
-                {
-                    if (elapsed > TimeSpan.FromHours(1))
-                    {
-                        throttler.ThrottleFactor--;
-                    }
-                    else
-                    {
-                        throttler.ThrottleFactor++;
-                    }
-                }
-            }
-        }
-
-        private static async Task Execute(Container container, CancellationToken stoppingToken)
-        {
-            var userPull = container.GetInstance<IUserPullService>();
-            var workoutPull = container.GetInstance<IWorkoutPullService>();
-            var achieveService = container.GetInstance<IAchievementService>();
-            var achievementPush = container.GetInstance<IAchievementPushService>();
-
-            foreach (var user in await userPull.Pull(stoppingToken))
-            {
-                var workouts = await workoutPull.Pull(user, stoppingToken);
-                var achievements = await achieveService.Process(user, workouts, stoppingToken);
-                await achievementPush.Push(achievements, stoppingToken);
+                _logger.LogInformation("Processing time: " + (DateTime.Now - start));
             }
         }
     }
