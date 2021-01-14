52479ecff01d63c25db06fc7a09c310d2fc34054
Nathan Baulch <nathan.baulch@gmail.com>
2021-01-14 16:23:42 +1100

    Remove SimpleInjector

--- source/Program.cs
+++ source/Program.cs
@@ -1,4 +1,7 @@
-﻿using FitBot.Logging;
+﻿using FitBot.Achievements;
+using FitBot.Logging;
+using FitBot.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,6 +14,21 @@ namespace FitBot
                 .ConfigureServices((context, services) =>
                 {
                     services.Configure<EmailLoggerOptions>(context.Configuration.GetSection("Email"));
+
+                    services.AddSingleton(context.Configuration.GetSection("Database").Get<DatabaseOptions>());
+                    services.AddSingleton(context.Configuration.GetSection("Fitocracy").Get<FitocracyOptions>());
+                    services.AddSingleton<IAchievementPushService, AchievementPushService>();
+                    services.AddSingleton<IAchievementService, AchievementService>();
+                    services.AddSingleton<IActivityGroupingService, ActivityGroupingService>();
+                    services.AddSingleton<IDatabaseService, DatabaseService>();
+                    services.AddSingleton<IFitocracyService, FitocracyService>();
+                    services.AddSingleton<IScrapingService, ScrapingService>();
+                    services.AddSingleton<IUserPullService, UserPullService>();
+                    services.AddSingleton<IWebRequestService, WebRequestService>();
+                    services.AddSingleton<IWorkoutPullService, WorkoutPullService>();
+                    services.Scan(scan => scan.FromEntryAssembly().AddClasses(filter => filter.AssignableTo<IAchievementProvider>()));
+                    services.Decorate<IWebRequestService, ThrottledWebRequestDecorator>();
+
                     services.AddHostedService<Worker>();
                 })
                 .ConfigureLogging(builder => builder.AddEmailLogger())
