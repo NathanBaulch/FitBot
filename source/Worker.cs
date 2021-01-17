551cb2656d3d638406ccc98c200cc1b4168d1526
Nathan Baulch <nathan.baulch@gmail.com>
2021-01-18 10:14:34 +1100

    Log unhandled exceptions and retry

--- source/Worker.cs
+++ source/Worker.cs
@@ -33,17 +33,30 @@ namespace FitBot
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                var start = DateTime.Now;
-
-                foreach (var user in await _userPull.Pull(stoppingToken))
+                try
                 {
-                    var workouts = await _workoutPull.Pull(user, stoppingToken);
-                    var achievements = await _achieveService.Process(user, workouts, stoppingToken);
-                    await _achievementPush.Push(achievements, stoppingToken);
-                    stoppingToken.ThrowIfCancellationRequested();
+                    var start = DateTime.Now;
+                    try
+                    {
+                        foreach (var user in await _userPull.Pull(stoppingToken))
+                        {
+                            var workouts = await _workoutPull.Pull(user, stoppingToken);
+                            var achievements = await _achieveService.Process(user, workouts, stoppingToken);
+                            await _achievementPush.Push(achievements, stoppingToken);
+                            stoppingToken.ThrowIfCancellationRequested();
+                        }
+                    }
+                    finally
+                    {
+                        _logger.LogInformation("Processing time: " + (DateTime.Now - start));
+                    }
                 }
-
-                _logger.LogInformation("Processing time: " + (DateTime.Now - start));
+                catch (Exception ex) when (ex.GetBaseException() is not OperationCanceledException)
+                {
+                    _logger.LogError(ex, ex.Message);
+                    _logger.LogInformation("Retrying in 10 seconds");
+                    await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                }
             }
         }
     }
